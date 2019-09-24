6f359d11c840f53b0fa45309f955295d51827ced
Carlos Alejandro Avilez Jimenez <carlos.avilez@softtek.com>
2019-09-23 23:32:09 -0700

    Adding css and sass to webpack project

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,15 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: "babel-loader"
       },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /(\.scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
