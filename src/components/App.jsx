0154fe7ae9c3f898b8cbce55083de96fd6564e80
Carlos Alejandro Avilez Jimenez <carlos.avilez@softtek.com>
2019-09-24 12:59:19 -0700

    Adding file loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.scss";
+// import logo from './react-logo.png'
 
 class App extends Component {
   render() {
     return (
       <div>
-
         <h1>Hello world</h1>
+        {/* <img src={logo} alt="" srcset=""/> */}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       {
         test: /(\.scss)$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: "file-loader"
       }
     ]
   },
