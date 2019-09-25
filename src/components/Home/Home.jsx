145ddbe88014a37c13f5f759a7051473efdbd0f9
Carlos Alejandro Avilez Jimenez <carlos.avilez@softtek.com>
2019-09-24 19:36:58 -0700

    Adding recipes list & Recipes details compoenents

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home Page
-            </div>
-        );
-    }
-}
+import React from "react";
+import RecipesDetails from "../recipes/RecipesDetails.jsx";
+import RecipesList from "../recipes/RecipesList.jsx";
+const Home = () => {
+  return (
+    <div style={{display:'flex'}}>
+      {" "}
+      <RecipesList />
+      <RecipesDetails />
+     
+    </div>
+  );
+};
 
 export default Home;
---
+++ src/components/recipes/RecipesList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const RecipesList = () => {
+  return (
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        <li>
+          <span>Creepy Haloween skulls Cupcakes</span>
+          <span>Dessert</span>
+        </li>
+        <li>
+          <span>Creepy Haloween skulls Cupcakes</span>
+          <span>Dessert</span>{" "}
+        </li>
+        <li>
+          <span>Creepy Haloween skulls Cupcakes</span>
+          <span>Dessert</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RecipesList;
